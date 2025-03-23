7a28721d1bb66e280e6c38c3e83e645810420abd
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-03-23 23:29:50 +0100

    update product model to make image field optional and add API endpoints for product creation and retrieval

--- models/product.model.js
+++ models/product.model.js
@@ -17,7 +17,7 @@ const ProductSchema = new mongoose.Schema({
     },
     image:{
         type: String,
-        required: true,
+        required: false,
     }
 },
 {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Product = require('./models/product.model.js');
 const { start } = require('./db.js');
 
 const app = express()
@@ -8,5 +9,23 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message}) 
+    }
+});
 
-start(app); 
+app.get('/api/products', async (req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+        console.log(products)
+    } catch (error) {
+        res.status(500).json({message: error.message}) 
+    }
+})
+
+start(app);  
