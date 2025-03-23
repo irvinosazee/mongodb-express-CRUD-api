d92ce05c81a39e904365e65e729826260dfc6d0d
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-03-23 22:10:58 +0100

    restructure server initialization and move MongoDB connection to db.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const app = express()
-const port = 3000
+const { start } = require('./db.js');
 
-app.listen(port, () => {
-    console.log(`Server is running on Port ${port}`)
-})
+const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-mongoose.connect(`${process.env.URI}`)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+
+start(app); 
