42085fe55c91c2c77238f2691cf78695eeec377c
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-03-25 00:26:10 +0100

    add CRUD API endpoints for products in server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const Product = require('./models/product.model.js');
 const { start } = require('./db.js');
 
 const app = express()
+start(app);  
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// Create a product
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body);
@@ -18,14 +20,52 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+// Get all products
 app.get('/api/products', async (req,res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products)
-        console.log(products)
+        // console.log(products)
     } catch (error) {
         res.status(500).json({message: error.message}) 
     }
 })
 
-start(app);  
+// Get a product by id
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+})
+
+// Update a product
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const productUpdate = await Product.findByIdAndUpdate(id, req.body, {new: true, runValidators: true});
+        if (!productUpdate){
+            return res.status(404).json({message : 'Product not found'})
+        }
+        res.status(200).json(productUpdate)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// Delete a product
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const productDelete = await Product.findByIdAndDelete(id);
+        if (!productDelete){
+            return res.status(404).json({message : 'Product not found'})
+        }
+        res.status(200).json({message : `Product with ID:${id} was deleted successfully `})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
