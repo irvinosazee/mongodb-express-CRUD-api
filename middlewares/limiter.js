e3091e38fe42d325c5d23aa42b9492df45b4d78f
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-03-26 17:35:51 +0100

    Add rate limiting middleware to restrict login attempts

---
+++ middlewares/limiter.js
@@ -0,0 +1,11 @@
+const rateLimit = require('express-rate-limit')
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
+module.exports = loginLimiter
