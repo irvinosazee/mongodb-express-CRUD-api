8c4a83096675e876c4e3b88b5c3f73f1338ff702
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-03-26 22:20:40 +0100

    ...

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit')
 const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 10,
-  message: 'Too many requests from this IP, please try again later.',
+  message: 'Too many requests login request from this IP, please try again later.',
   standardHeaders: true,
   legacyHeaders: false,
 })
